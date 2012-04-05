b753a09078b96ee79473fe612dc3953f987c59f0
Alastair Coote <alastair@urdb.org>
2012-04-05 16:22:57 -0400

    again

--- html/js/jukebox.js
+++ html/js/jukebox.js
@@ -403,7 +403,6 @@
         doTweet = confirm("You have no credits left! You can earn more credits by tweeting about StartupBus and JukeMob (and getting your friends to retweet you!). Do it now?");
         if (doTweet) {
           twitMsg = "I'm using @jukemobapp to control the playlist at the NYC @startupbus demo day!";
-          twitMsg = "@jukemobapp " + twitMsg;
           if (aLink) {
             twitMsg += " I just queued up \"{1}\"";
             hashtag = "jm81" + jukebox.User.currentUserId;
